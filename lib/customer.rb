0ee9caa424634c1dd6e85dbd0c46d3e995450ccd
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-23 16:47:08 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -19,7 +19,7 @@ attr_accessor :meals
   end
   def waiters
     customer_waiters = Meal.all.find_all{ |meal|
-    meal.waiter == self.new_meal.waiter
+    meal.waiter == self.meals.waiter
   return meal.waiter}
   binding.pry
     customer_waiters
