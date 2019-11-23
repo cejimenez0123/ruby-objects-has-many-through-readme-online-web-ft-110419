9a9f7b95e83327f5051923dff9b0b176a604516d
Christian Jimenez <christianjimenez0123@gmail.com>
2019-11-23 16:42:05 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,15 @@
 class Waiter
-
+    attr_reader :name, :experience
+    @@all = []
+  def initialize(name,experience)
+    @name = name
+    @experience = experience
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def waiter
+    @@all[0]
+  end
 end
